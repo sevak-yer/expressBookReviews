bb061cac8e62d29c4d9930547e501589951145e2
Sevak Yeressian <yeressian@gmail.com>
2024-08-15 09:12:13 -0400

    Add logic to /, /isbn/:isbn and /author/:author general routes Add auth code to index.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,33 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send({books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_book = {}
+    for (let key in books) {
+        if (books[key].isbn === isbn) {
+            filtered_book = books[key];
+            break;
+        }
+    }
+    res.send(filtered_book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let book;
+    for (let key in books) {
+        if (books[key].author === author) {
+            book = books[key];
+            break;
+        }
+    }
+    res.send(book);
 });
 
 // Get all books based on title
